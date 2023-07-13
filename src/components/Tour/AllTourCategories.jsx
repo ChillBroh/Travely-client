eab85a3cacec6f723e3572ef90701a9a5bdcbc90
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-07-13 16:21:29 +0530

    undo

--- src/components/Tour/AllTourCategories.jsx
+++ src/components/Tour/AllTourCategories.jsx
@@ -18,9 +18,7 @@ const Beach = () => {
 
   const getTours = async () => {
     try {
-      const response = await axios.get(
-        "https://travely-43dfb076364f.herokuapp.com/api/tours"
-      );
+      const response = await axios.get("/tours");
       const tours = response.data.filter((tour) => {
         const category = tour.category.toLowerCase().replace(/\s+/g, "");
         console.log("category", category);
