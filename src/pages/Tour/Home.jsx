7ef432d56d070f6671950eb0ddfe67b900472557
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-10-18 16:23:56 +0530

    external link added for google analytics

--- src/pages/Tour/Home.jsx
+++ src/pages/Tour/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
         <ol class="list-reset flex">
           <li>
             <Link
-              to={"/"}
+              to={"https://www.tripadvisor.com/"}
               class="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
             >
               Home
